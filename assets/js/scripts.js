9f438322d3c616442207e8f3578d5239d0949ee9
Farhan Mullick <38060872+Farhansadiqmullick@users.noreply.github.com>
2023-06-29 16:09:33 +0600

    multistep form design

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -59,10 +59,117 @@ window.addEventListener("DOMContentLoaded", (event) => {
    */
 
   var myModal = new bootstrap.Modal(document.getElementById("myModal"));
-  myModal.show();
+  if (myModal) {
+    myModal.show();
+    var closeButton = document.querySelector(".modal .close");
+    closeButton.addEventListener("click", function () {
+      myModal.hide();
+    });
+  }
+});
 
-  var closeButton = document.querySelector(".modal .close");
-  closeButton.addEventListener("click", function () {
-    myModal.hide();
+document.addEventListener("DOMContentLoaded", function () {
+  const steps = Array.from(document.querySelectorAll(".step"));
+  const nextBtn = document.querySelectorAll("form.dashboard .next");
+  const prevBtn = document.querySelectorAll("form.dashboard .prev");
+  const navItem = document.querySelectorAll("form.dashboard .nav-item");
+  const form = document.querySelector("form.dashboard");
+
+  var currentTab = 0; // Current tab is set to be the first tab (0)
+  showTab(currentTab); // Display the current
+
+  // function changeStep(btn) {
+  //   let index = 0;
+  //   const active = document.querySelector(".active");
+  //   index = steps.indexOf(active);
+  //   steps[index].classList.remove("active");
+  //   if (btn === "next") {
+  //     index++;
+  //   } else if (btn === "prev") {
+  //     index--;
+  //   }
+  //   steps[index].classList.add("active");
+  // }
+
+  function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i,
+      x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].className = x[i].className.replace(" active", "");
+      x[i].addEventListener("click", clickHandler(i));
+    }
+    //... and adds the "active" class on the current step:
+    x[n].className += " active";
+
+    function clickHandler(index) {
+      return function() {
+        if (index !== currentTab) {
+          nextPrev(index - currentTab);
+        }
+      };
+    }
+  }
+
+  function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("step");
+    // Exit the function if any field in the current tab is invalid:
+    // if (n == 1 && !validateForm()) return false;
+    // Hide the current tab:
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form...
+    if (currentTab >= x.length) {
+      // ... the form gets submitted:
+      document.getElementById("dashboard").submit();
+      return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+  }
+
+  function showTab(n) {
+    // This function will display the specified tab of the form...
+    var x = document.getElementsByClassName("step");
+    x[n].style.display = "block";
+    //... and fix the Previous/Next buttons:
+    document.getElementById("submit").style.display = "none";
+    if (n == 0) {
+      document.getElementById("prev").style.display = "none";
+    } else {
+      document.getElementById("prev").style.display = "inline";
+    }
+    if (n == x.length - 1) {
+      document.getElementById("next").style.display = "none";
+      document.getElementById("submit").style.display = "inline";
+    } else {
+      document.getElementById("next").style.display = "inline";
+    }
+    //... and run a function that will display the correct step indicator:
+    fixStepIndicator(n);
+  }
+
+  nextBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      nextPrev(1);
+    });
+  });
+  prevBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+      nextPrev(-1);
+    });
+  });
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputs = [];
+    form.querySelectorAll("input").forEach((input) => {
+      const { name, value } = input;
+      inputs.push({ name, value });
+    });
+    console.log(inputs);
+    form.reset();
   });
 });
